532bc94fb9396a704b682c97cfb53db80e1e818d
essential-robot-4364 <brandon.james414@gmail.com>
2019-09-29 15:30:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
-    :montague => 1
-      :patriarch 
+    :montague => {1},
+      :patriarch => {2},
+      :matriarch => {3},
+      :heroine => {4},
+      :heroine_friends => {}
       #your key/value pairs here!
       }, 
     :capulet => {
